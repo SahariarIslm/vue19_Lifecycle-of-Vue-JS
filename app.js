647cac47803cbb850f5eaaffc3c312ec0f27e972
Sahariar Islam <dhruv.islam7584@gmail.com>
2022-01-17 09:54:57 +0600

    vue9_loop_v-for

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 new Vue({
     el: "#app",
     data:{
-        user: "Luffy",
-        userAge: 16,
-        allowedAge: 18,
-        
-    },
-    methods: {
-        changeuser() {
-            return this.user==="Luffy" ? true : false;
-        },
-    },
+        cars: ['BMW','Ford','Toyota'],
+        user: {
+            name: 'Sabit',
+            age: 25,
+            country: 'Bangladesh'
+        }
+    }
 });
