8801bb69871a7eaecbe9fdce7690bd958dd6787f
Sahariar Islam <dhruv.islam7584@gmail.com>
2022-01-25 08:09:18 +0600

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 new Vue({
     el: "#app",
     data:{
-        formData: {
-            firstName:'',
-            lastName :''
-        },
+        a:0,
+        b:0,
+        salary:10
     },
     methods: {
-        handleForm(){
-            console.log(this.formData);
-        }, 
+        // addToA(){
+        //     console.log('add a');
+        //     return this.a + this.salary;
+        // },
+        // addToB(){
+        //     console.log('add b');
+        //     return this.b + this.salary;
+        // }
+    },
+    computed: {
+        caddToA(){
+            console.log('add a');
+            return this.a + this.salary;
+        },
+        caddToB(){
+            console.log('add b');
+            return this.b + this.salary;
+        }
     }
-});
+});                        
